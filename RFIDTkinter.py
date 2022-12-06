2f9f93d1214b8fb23064d8c2783ee2d4ada81ca9
jusbro <97056196+jusbro@users.noreply.github.com>
2022-12-06 14:35:51 -0500

    Update RFIDTkinter.py

--- RFIDTkinter.py
+++ RFIDTkinter.py
@@ -28,9 +28,64 @@ window.title("Pass System V 0.3")
 window.geometry('1920x1080')
 window.update_idletasks()
 window.update
+homeTitleLabel = Label(window, text="Scan a Pass", font=("Arial Bold", 150))
+nameEntry = Entry(window, bd="4", relief = SUNKEN, font=("Arial Bold", 100))
+nameSubmitButton = Button(window)
+homeTitleLabel.pack()
+nameEntry.pack()
+nameEntry.pack_forget()
+
 
 #VARIABLES AND CONSTANTS
 versionNumber = 0.3
+signOutName = ""
+
+boysPass1 = 584186286068
+boysPass1Stat = "in"
+boysPass1Name = ""
+boysPass2 = 584183328681
+boysPass2Stat = "in"
+boysPass2Name = ""
+
+girlsPass1 = 584189224911
+girlsPass1Stat = "in"
+girlsPass1Name = ""
+girlsPass2 = 584194599187
+girlsPass2Stat = "in"
+girlsPass1Name = ""
+
+printer1 = 584183531207
+printer1Stat = "in"
+printer1Name = ""
+printer2 = 584188180704
+printer2Stat = "in"
+printer2Name = ""
+printer3 = 584191527234
+printer3Stat = "in"
+printer3Name = ""
+printer4 = 11
+printer4Stat = "in"
+printer4Name = ""
+
+locker1 = 584192701728
+locker1Stat = "in"
+locker1Name = ""
+locker2 = 584194080866
+locker2Stat = "in"
+locker2Name = ""
+locker3 = 584184701107
+locker3Stat = "in"
+locker3Name = ""
+locker4 = 584189885919
+locker4Stat = "in"
+locker4Name = ""
+
+adminWhiteCard = 837979804343
+adminWhiteCardStat = "out"
+dailyCounter = 1
+
+#buzzerPin = -1
+#blinkLED = -1
 
 #METHODS
 def screenUpdate():
@@ -43,7 +98,7 @@ def createWindow():
     window.title("Pass System V"+ versionNumber)
     window.geometry('1920x1080')
     screenUpdate()
-
+"""
 def bootUpScreen():
     #A bootup screen that displays when the program first launches
     print("Bootup Screen")
@@ -64,16 +119,42 @@ def bootUpScreen():
     #This will 'hide' the labels
     titleLabel.forget()
     secLabel.forget()
+"""
+def storeName():
+    print("Storing Name")
+    signOutName = nameEntry.get()
+    print(signOutName)
+    nameSubmitButton.pack_forget()
+    nameEntry.pack_forget()
+    try:
+        strCurrentTime = getTimeStamp()
+    except:
+        print("Error signing out in the StoreName Method")
+    screenUpdate()
+    homeScreen()
 
 def signOut():
     print("Signing Out")
+    homeTitleLabel.config(text="Signing Out")
+    homeTitleLabel.pack()
+    nameEntry.pack()
+    print("1")
+    #nameSubmitButton.config(command=storeName())
+    nameSubmitButton.config(text = "Sign Out", bd='5',font=("Arial Bold", 75), command=storeName)
+    print("1.5")
+    nameSubmitButton.pack()
+    print("2")
+    window.mainloop()
+    time.sleep(0.5)
+    #nameSubmitButton.pack_forget()
 
 def signIn():
     print("Signing In")
 
 def homeScreen():
     print("Home Screen")
-    homeTitleLabel = Label(window, text="Scan a Pass", font=("Arial Bold", 150))
+    
+    homeTitleLabel.config(text="Scan a Pass")
     #homeTitleLabel.grid(column=0, row=0)
     #homeTitleLabel.place(relx=0.5, anchor=N)
     homeTitleLabel.pack()
@@ -92,6 +173,7 @@ def RFIDScan():
             print("Looking for Data")
             id, text = reader.read()
             print("Got Data")
+            signOut()
     except:
         print("Error Reading RFID, Fell out of RFID Scan Method")
 
@@ -99,7 +181,13 @@ def createFileLog():
     #work in progress
     #Creates the log (currently a txt) if the current file is nonexistant, otherwise it reopens the exisiting log file (in the event the Pi shuts off)
     #Files will be named by date (requiring a call to the getDate() method)
-    print("Create File Log")
+    try:
+        log = open("signoutSheet.txt", "w")
+        log.write("Sign Out Sheet\n")
+        log.close()
+        print("Created File Log Successfully")
+    except:
+        print("Failed to create File Log")
 
 def getTimeStamp():
     #Gets the current time using the time library. Returns time in the form of hours minutes seconds
@@ -122,6 +210,6 @@ def displayLogSheet():
 
 
 #Main Loop
-bootUpScreen()
+#bootUpScreen()
 createFileLog()
 homeScreen()
